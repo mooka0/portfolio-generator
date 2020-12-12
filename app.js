6275759be8477881ad3e91bfd2b5ca7d180afeb9
mooka0 <angelankao@gmail.com>
2020-12-11 17:15:37 -0800

    inquierer package achieve first question prompt

--- app.js
+++ app.js
@@ -1,32 +1,21 @@
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
+// const pageHTML = generatePage(name, github);
 
-// const printProfileData = profileDataArr => {
-  // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-//   console.log('================');
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
-
-
-const fs = require('fs');
-
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-
-
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
