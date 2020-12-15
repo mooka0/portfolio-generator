6e86f0a3a9bb9db2c2798ee583bf66e7159b3379
mooka0 <angelankao@gmail.com>
2020-12-14 16:22:33 -0800

    html generation complete

--- app.js
+++ app.js
@@ -131,8 +131,8 @@ promptUser()
   .then(portfolioData => {
 
     const pageHTML = generatePage(portfolioData);
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
